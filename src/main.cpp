5e7d4ceeb2191f6cd28132260a040b6f7e1b7e15
Tobias Schmid <2toschmid@googlemail.com>
2018-01-26 15:50:31 +0100

    node struct & get addresses

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
-#include <Arduino.h>
+
 #include <Ethernet.h>
 #include <SPI.h>
-//#include "RestClient.h"
+
+// wifi & http
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+
+// json parsing
 #include <ArduinoJson.h>
-#include <SD.h>
+
+// DS18B20 sensor
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#include <Arduino.h>
+
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS D1
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire one_wire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&one_wire);
 
 #define HTTP_DEBUG
 
+// TODO:multiple ssids/wlans
 const char* json_config = "{"
   "\"host\": \"api-v1.teufelswerk-berlin.de\","
   "\"port\": 443,"
@@ -17,12 +33,15 @@ const char* json_config = "{"
   "\"sha_1\": \"88:49:DD:29:76:84:DF:E3:3E:60:81:5E:7E:4A:D9:88:77:C1:67:9D\""
 "}";
 
-// RestClient client = RestClient("api-v1.teufelswerk-berlin.de", 443, 1);
 HTTPClient client;
 
 enum method {
   POST,
   GET
+};
+enum unity {
+  celsius,
+  fahrenheit
 };
 
 // Configuration that we'll store on disk
@@ -39,11 +58,16 @@ struct Node {
   String pw;
   String email;
   int id;
-} node;
 
-struct Sensor {
-  String mac;
-} sensor;
+  struct Sensor {
+    String mac; // String representation of addr
+    DeviceAddress addr;
+    int id;
+    std::vector<String> data;
+  };
+  std::vector<Node::Sensor> sensors;
+
+} node;
 
 struct Token {
   String access_token;
@@ -52,27 +76,37 @@ struct Token {
   String uid;
 } token;
 
-bool parse_json(JsonObject* json, const char* str){
+void get_sensors_mac(){
+  DeviceAddress mac;
 
-  // Allocate the memory pool on the stack.
-  // Don't forget to change the capacity to match your JSON document.
-  // Use arduinojson.org/assistant to compute the capacity.
-  StaticJsonBuffer<512> jsonBuffer;
-  // Parse the root object
-  json = &jsonBuffer.parseObject(str);
-  if (!json->success())
-    return false;
-  return true;
+  while ( one_wire.search(mac)) {
+    char macStr[24] = { 0 };
+    sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6], mac[7]);
+
+    Node::Sensor sensor;
+    sensor.mac = String(macStr);
+    memcpy( sensor.addr, mac, sizeof(DeviceAddress) );
+    node.sensors.push_back(sensor);
+  }
+
+  one_wire.reset_search();
+}
+
+// initialize
+void initialize(){
+  // get node settings
+  node.mac = WiFi.macAddress();
+  node.pw = "123123123"; // TODO
+  node.email = "test@test.de"; // TODO
+
+  get_sensors_mac();
 }
 
 // Loads the configuration from a file
 void load_config(const char* json_config) {
 
   // Allocate the memory pool on the stack.
-  // Don't forget to change the capacity to match your JSON document.
-  // Use arduinojson.org/assistant to compute the capacity.
-  StaticJsonBuffer<512> jsonBuffer;
-
+  StaticJsonBuffer<512> jsonBuffer; //TODO: capacity: arduinojson.org/assistant
   // Parse the root object
   JsonObject &root = jsonBuffer.parseObject(json_config);
 
@@ -85,13 +119,6 @@ void load_config(const char* json_config) {
   strlcpy(config.wifi_ssid, root["wifi_ssid"] | "" ,sizeof(config.wifi_ssid));
   strlcpy(config.wifi_pw, root["wifi_pw"] | "" ,sizeof(config.wifi_pw));
   strlcpy(config.sha_1, root["sha_1"] | "" ,sizeof(config.sha_1));
-
-  node.mac = "a1b2c3d4e5f6g7h8"; // TODO
-  node.pw = "123123123"; // TODO
-  node.email = "test@test.de"; // TODO
-
-  sensor.mac = "i1j2k3l4m5n6o7p8"; // TODO
-
 }
 
 void connect_wlan() {
@@ -199,38 +226,103 @@ bool login() {
     node.id = get_id(response);
     return true;
   }
-
   return false;
 }
 
 String get_node_data() {
-  String request_body = "{\"mac\":\"" + node.mac + "\",\"password\":\"" + node.pw + "\"}";
   String path = "/nodes/" + String(node.id);
   return request(GET, const_cast<char*>(path.c_str()));
+}
+
+bool create_sensor(Node& node, Node::Sensor& sensor) {
+  String request_body = "{\"mac\":\"" + sensor.mac + "\"}";
+  String path = "/nodes/" + String(node.id) + "/sensors/";
+  String response = request(POST, const_cast<char*>(path.c_str()), request_body);
+  if (response.length()){
+    sensor.id = get_id(response);
+    return true;
+  }
+  return false;
+}
+
+bool create_sensors(Node& node) {
+  for(int i = 0; i < node.sensors.size(); i++){
+    create_sensor(node, node.sensors[i]);
+  }
+}
+
+bool push_sensor_data(String* sensor_data,int data_count, String sensor_mac) {
+  // Allocate the memory pool on the stack.
+  StaticJsonBuffer<4000> jsonBuffer; //TODO: capacity: arduinojson.org/assistant
+  JsonObject& _json = jsonBuffer.createObject();
+  JsonArray& _sensors = _json.createNestedArray("sensors");
+
+  JsonObject& _s = _sensors.createNestedObject();
+  _s["mac"] = sensor_mac;
+  // JsonArray& _sensor_data = _s.createNestedArray("sensor_data");
+  // for(int i = 0; i < data_count; i++){
+  //   _sensor_data.add(sensor_data[i]);
+  // }
+  _json.printTo(Serial);
+  // String request_body = "{\"mac\":\"" + sensor.mac + "\"}";
+  // String path = "/nodes/" + String(node.id) + "/sensor_data/";
+  // String response = request(POST, const_cast<char*>(path.c_str()), request_body);
+
+}
+
+// TODO: with created_at
+String json_data(float value, String u) {
+    // Allocate the memory pool on the stack.
+    StaticJsonBuffer<512> jsonBuffer; //TODO: capacity: arduinojson.org/assistant
+    JsonObject& json = jsonBuffer.createObject();
+    json["value"] = value;
+    json["unity"] = u;
+    String json_data = "";
+    json.printTo(json_data);
+    return json_data;
 }
 
 
 
 void setup() {
-    Serial.setDebugOutput(true);
+    // Serial.setDebugOutput(true);
     // put your setup code here, to run once:
     Serial.begin(9600);
 
+    // Start up the library
+    sensors.begin();
+
     // Should load default config if run for the first time
-    Serial.println(F("Loading configuration..."));
+    //Serial.println(F("Loading configuration..."));
     load_config(json_config);
 
-    connect_wlan();
+    initialize();
+    //connect_wlan();
 
-    if(!login()){
-      create_login();
-    }
+    // if(!login()){
+    //   create_login();
+    // }
+    // create_sensor();
 }
 
 
 void loop() {
-  login();
-  Serial.print(get_node_data());
+  //login();
+  //Serial.print(get_node_data());
+  Serial.println("start loop");
+
+  Serial.println(node.mac);
+  for(int i = 0; i < node.sensors.size(); i++){
+    Serial.println(node.sensors[i].mac);
+  }
+  // request sensor data
+  // sensors.requestTemperatures(); // Send the command to get temperature readings
+  // float temp = sensors.getTempCByIndex(0);
+  // // push sensor data to buffer
+  // sensor.data[sensor.data_count] = json_data(temp, "celsius");
+  // sensor.data_count ++;
+  //
+  // push_sensor_data(sensor.data,sensor.data_count, sensor.mac);
   delay(1000);
 
 }
